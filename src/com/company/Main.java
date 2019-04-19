805a75cb2572c8b1141d48ec94673d2e76d1ae2a
Cesar Mejia <crsmejia93@gmail.com>
2019-04-19 13:11:57 -0400

    Added README file

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,7 +6,6 @@ public class Main {
 
     public static void main(String[] args) {
 	    int range;
-	    int remainder;
         Scanner input = new Scanner(System.in);
 
         System.out.print("Enter a range, to find the prime numbers: ");
